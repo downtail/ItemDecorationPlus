2ceb85cc314cda5deac96409d2f752a2b20e6fe5
downtail <916382412@qq.com>
2019-08-13 16:19:51 +0800

    8.13commit

--- sample/src/main/java/com/downtail/itemdecorationplus/masked/MaskedActivity.java
+++ sample/src/main/java/com/downtail/itemdecorationplus/masked/MaskedActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
